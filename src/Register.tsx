9956f283bb1e2e879f30611f39ae59caef5f9497
Nikhil <nikhilsimontoppo@gmail.com>
2022-12-27 09:40:31 +0530

    finished up signin hook and page

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,45 +1,26 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { auth } from "./firebase";
+import { useState } from "react";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { useSignup } from "./hooks/useSignup";
 
 function Register() {
-  const navigate = useNavigate();
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    pass: "",
-  });
-  const [errorMsg, setErrorMsg] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmission = () => {
-    if (!values.name || !values.email || !values.pass) {
-      setErrorMsg("Fill all fields");
-      return;
-    }
-    setErrorMsg("");
 
-    createUserWithEmailAndPassword(auth, values.email, values.pass)
-      .then(async (res) => {
-        const user = res.user;
-        await updateProfile(user, {
-          displayName: values.name,
-        });
-        navigate("/");
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log("Error-", err);
-        setErrorMsg(err.message);
-      });
+  const { signup, isPending, error } = useSignup();
+  const { dispatch } = useAuthContext();
 
-    console.log(values);
+  const handleSubmission = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    signup(email, password, displayName);
+    console.log(email, password, displayName);
   };
 
   return (
     <div className="App flex flex-col items-center justify-center">
       <form
-        action=""
+        onSubmit={handleSubmission}
         className="border p-5 w-[400px] flex flex-col justify-center items-center mt-10  gap-2 bg-gray-300 rounded-md"
       >
         <h1 className="text-2xl font-bold">Registration</h1>
@@ -49,27 +30,22 @@ function Register() {
           className="px-2"
           placeholder="Name"
           type="text"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, name: event.target.value }))
-          }
+          onChange={(e) => setDisplayName(e.target.value)}
         />
         <span>Email</span>
         <input
           className="px-2"
           placeholder="Email"
           type="email"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, email: event.target.value }))
-          }
+          onChange={(e) => setEmail(e.target.value)}
         />
         <span>Password</span>
         <input
           className="px-2"
           placeholder="Password"
           type="password"
-          onChange={(event) =>
-            setValues((prev) => ({ ...prev, pass: event.target.value }))
-          }
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <span>Mobile Number</span>
@@ -80,14 +56,18 @@ function Register() {
         <input className="px-2" placeholder="School" type="text" />
         <span>How long have you been playing cricket?</span>
         <input className="px-2" placeholder="Time" type="text" />
-        <span className="text-red-500 font-semibold">{errorMsg}</span>
+        <span className="text-red-500 font-semibold">{error}</span>
         <span className="flex gap-4 mt-4">
-          <div
-            onClick={handleSubmission}
-            className="hover:cursor-pointer border-green-500 bg-green-400 border px-5 py-1 rounded-lg transition-all  hover:bg-green-600"
-          >
-            Register
-          </div>
+          {!isPending && (
+            <button className="hover:cursor-pointer border-green-500 bg-green-400 border px-5 py-1 rounded-lg transition-all  hover:bg-green-600">
+              Register
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              loading
+            </button>
+          )}
         </span>
       </form>
     </div>
